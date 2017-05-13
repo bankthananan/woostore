0ed3edd7558a50ca220a7542656a4985b286a1c9
Iammmiind <Onanong.pureemahawong@gmail.com>
2017-05-13 17:37:09 +0700

    [Front-end] complete layout login component

--- frontend/src/app/product/product-routing.module.ts
+++ frontend/src/app/product/product-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ListProductComponent} from './list-product/list-product.component';
+import {AddProductComponent} from './add-product/add-product.component';
 
 const productRoutes: Routes = [
-  {path: 'list', component: ListProductComponent}
+  {path: 'list', component: ListProductComponent},
+  {path: 'add', component: AddProductComponent}
 ];
 
 @NgModule({
