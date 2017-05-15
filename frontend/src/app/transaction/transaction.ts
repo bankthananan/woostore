e0c20c74afdcd48f8cd63cd12efeee7b1c8a2372
Zenon 'SI <si.zenon@gmail.com>
2017-05-15 14:00:47 +0700

    [Frontend] Make more beautiful

--- frontend/src/app/transaction/transaction.ts
+++ frontend/src/app/transaction/transaction.ts
@@ -1,12 +1,13 @@
 import {OrderItem} from './order-item';
 import {User} from '../user/user';
 import {TransactionStatus} from './transaction-status';
+import {WooPayment} from './woo-payment';
 export class Transaction {
   id: number;
   items: OrderItem[];
   status: TransactionStatus;
   owner: User;
-  // wooPayment
+  wooPayment: WooPayment;
   date: Date;
   totalPrice: number;
 }
