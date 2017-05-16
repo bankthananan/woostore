dae38eb970c493672bb830925c31492bb8c498ec
Zenon 'SI <si.zenon@gmail.com>
2017-05-16 15:38:55 +0700

    [Backend] Fix Deploy bug !!

--- src/main/java/com/woostore/config/security/SecurityApplicationInitializer.java
+++ src/main/java/com/woostore/config/security/SecurityApplicationInitializer.java
@@ -1,12 +1,14 @@
 package com.woostore.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.multipart.support.MultipartFilter;
 
 import javax.servlet.ServletContext;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
     @Override
     protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
